d70aa8c84896698b63d082c203f89e31910c03f5
Furkan A <asw13tr@gmail.com>
2020-08-07 11:26:05 +0200

    development ayarları

--- middlewares/asw-admin-login-control.js
+++ middlewares/asw-admin-login-control.js
@@ -3,14 +3,16 @@ let boolSession = (session)=>{
 }
 
 module.exports = (request, response, next) => {
-    request.session.user = {
-        id: 1,
-        username: 'atabas61',
-        email: 'atabas61@gmail.com',
-        level: 3,
-        last_login: '2020-08-02T23:37:01.000Z'
+    if(process.env.NODE_ENV != 'production'){
+        request.session.user = {
+            id: 1,
+            username: 'atabas61',
+            email: 'atabas61@gmail.com',
+            level: 3,
+            last_login: '2020-08-02T23:37:01.000Z'
+          }
+          request.session.save();
       }
-      request.session.save();
     if(request.originalUrl.indexOf('admin/login') > -1){ // Login sayfasındasın
         if(boolSession(request.session)==true){
             response.redirect('/admin');
